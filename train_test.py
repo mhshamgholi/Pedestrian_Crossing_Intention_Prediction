e520b3bf143ed47d26bcd1f24d8eb6c0bb49f1e6
mhshamgholi <88778595+mhshamgholi@users.noreply.github.com>
2023-10-09 04:50:08 +0330

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -32,8 +32,8 @@ for gpu in gpus:
 
 
 # path to JAAD dataset, please change to your local path
-path_jaad = "/home/steven/submission_T_IV/JAAD"
-path_pie = "/media/steven/MEDIA/PIE"
+path_jaad = "./JAAD"
+path_pie = "./PIE"
 
 # config = tf.compat.v1.ConfigProto()
 # # config.gpu_options.per_process_gpu_memory_fraction=0.8
